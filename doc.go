3482e6342a533dad51c8321f6a5c99187a8d22e1
Jason Song <i@wolfogre.com>
2024-09-06 15:28:55 +0800

    chore: typo

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 // protomagic provides utilities for effortless and magical gRPC proto manipulation.
-// It contains the following packages:
+// It contains the following modules:
 //   - enummagic: utilities for enum types.
 //   - fieldmaskmagic: utilities for fieldmask types.
 //
